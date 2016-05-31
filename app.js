e46654a8d6564451b9c51a7ca5e109aa81fa2932
wacily <wacily@163.com>
2016-06-01 01:51:20 +0800

    添加編輯頁面，同時設置富文本編輯器

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ueditor=require('ueditor');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +12,8 @@ var detail = require('./routes/detail');
 var login = require('./routes/login');  //user login page
 var mag_index = require('./routes/mag_index');  //manage index page
 var mag_register = require('./routes/mag_register');
+var mag_edit = require('./routes/mag_edit');
+var mag_setting = require('./routes/mag_setting');
  
 var app = express();
 
@@ -31,6 +34,29 @@ app.use('/detail',detail);
 app.use('/manage/login', login);
 app.use('/manage/index',mag_index);
 app.use('/manage/register',mag_register);
+app.use('/manage/edit',mag_edit);
+app.use('/manage/setting',mag_setting);
+app.use("/lib/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if(req.query.action === 'uploadimage'){
+        var foo = req.ueditor;
+        var date = new Date();
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/lib/ueditor/image';
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage'){
+        var dir_url = '/lib/ueditor/image';
+        res.ue_list(dir_url);  // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/lib/ueditor/config.json')
+    }}));
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
